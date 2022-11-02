0aa593d46b479b28c1a3ccc62427fc5926c798f9
Benjamin PREVOT <me@benjaminprevot.fr>
2022-11-02 22:36:51 +0100

    docs: Add Javadoc comments

--- src/main/java/rocks/cleancode/hamcrest/file/IsFileMatcher.java
+++ src/main/java/rocks/cleancode/hamcrest/file/IsFileMatcher.java
@@ -7,8 +7,20 @@ import org.hamcrest.TypeSafeMatcher;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/**
+ * Matches a file.
+ *
+ * @since 1.0.0
+ */
 public class IsFileMatcher extends TypeSafeMatcher<Path> {
 
+    /**
+     * Create a matcher for an existing file.
+     *
+     * @return Existing file matcher
+     *
+     * @since 1.0.0
+     */
     public static Matcher<Path> file() {
         return new IsFileMatcher();
     }
@@ -16,16 +28,25 @@ public class IsFileMatcher extends TypeSafeMatcher<Path> {
     private IsFileMatcher() {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected boolean matchesSafely(Path path) {
         return Files.isRegularFile(path);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void describeTo(Description description) {
         description.appendText("a file");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void describeMismatchSafely(Path path, Description mismatchDescription) {
         mismatchDescription.appendText("was not a file");
