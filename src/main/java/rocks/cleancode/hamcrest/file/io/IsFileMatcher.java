54bcba3039dc65a184394691aab601fd7ff9efc5
Benjamin PREVOT <me@benjaminprevot.fr>
2022-11-02 23:32:20 +0100

    docs: Create Javadoc comments

--- src/main/java/rocks/cleancode/hamcrest/file/io/IsFileMatcher.java
+++ src/main/java/rocks/cleancode/hamcrest/file/io/IsFileMatcher.java
@@ -6,8 +6,20 @@ import org.hamcrest.TypeSafeMatcher;
 
 import java.io.File;
 
+/**
+ * Matches a file.
+ *
+ * @since 1.0.0
+ */
 public class IsFileMatcher extends TypeSafeMatcher<File> {
 
+    /**
+     * Create a matcher for an existing file.
+     *
+     * @return Existing file matcher
+     *
+     * @since 1.0.0
+     */
     public static Matcher<File> file() {
         return new IsFileMatcher();
     }
@@ -15,16 +27,25 @@ public class IsFileMatcher extends TypeSafeMatcher<File> {
     private IsFileMatcher() {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected boolean matchesSafely(File file) {
         return file.isFile();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void describeTo(Description description) {
         description.appendText("a file");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void describeMismatchSafely(File file, Description mismatchDescription) {
         mismatchDescription.appendText("was not a file");
