eea56b7f4f2cdf0df5987482bf2877739c275b33
Benjamin PREVOT <me@benjaminprevot.fr>
2022-11-02 23:23:37 +0100

    refactor: Move package description

---
+++ src/main/java/rocks/cleancode/hamcrest/file/nio/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Hamcrest matchers for files and directories in {@code java.nio.file} package.
+ */
+package rocks.cleancode.hamcrest.file.nio;
--- src/main/java/rocks/cleancode/hamcrest/file/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Hamcrest matchers for files and directories.
- */
-package rocks.cleancode.hamcrest.file;
